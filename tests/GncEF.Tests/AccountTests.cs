c66097ac96ce0ed6e3b9134f487e68dc21f37b8a
David Chappelle <david@codermonkey.com>
2026-08-04 22:08:53 -0400

    added test database and tests against it

---
+++ tests/GncEF.Tests/AccountTests.cs
@@ -0,0 +1,69 @@
+using Xunit;
+using GncEF;
+using System.Data.Common;
+
+namespace GncEF.Tests;
+
+public class AccountTests : IDisposable
+{
+    private readonly GncContext _db;
+
+    public AccountTests()
+    {
+        _db = new GncContext(Path.Combine("data", "Test.gnucash"));
+    }
+    
+    [Fact]
+    public void AccountValue()
+    {
+        var account = _db.AccountFromAbsolutePath("Assets:Current Assets:Cash in Wallet");
+        Assert.NotNull(account);
+        
+        var currentBalance = _db.GetAccountValue(account);
+        Assert.Equal((8750L, 100L), currentBalance);
+    }
+
+    [Fact]
+    public void AccountValueWithRange()
+    {
+        var account = _db.AccountFromAbsolutePath("Expenses:Dining");
+        Assert.NotNull(account);
+        
+        // as of today
+        var currentBalance = _db.GetAccountValue(account, asOfDate: null);
+        Assert.Equal((5337L, 100L), currentBalance);
+
+        // as of 2026-08-01
+        var bal2 = _db.GetAccountValue(account, asOfDate: new DateOnly(2026,8,1));
+        Assert.Equal((2593L, 100L), bal2);
+
+
+    }
+
+    [Fact]
+    public void AccountValueChangeByMonth()
+    {
+        // change in value for 2026-08
+        var startDate = new DateOnly(2026,8,1);
+        var endDate = startDate.AddMonths(1).AddDays(-1);
+
+        var accountDining = _db.AccountFromAbsolutePath("Expenses:Dining");
+        Assert.NotNull(accountDining);
+        var amtSpent = _db.GetAccountValueChange(accountDining, startDate, endDate);
+        Assert.Equal((4087L, 100L), amtSpent);
+
+        var accountCash = _db.AccountFromAbsolutePath("Assets:Current Assets:Cash in Wallet");
+        Assert.NotNull(accountCash);
+        var amtCashChange = _db.GetAccountValueChange(accountCash, startDate, endDate);
+        Assert.Equal((-4087L, 100L), amtCashChange);
+
+        Assert.Equal(amtSpent.num, -amtCashChange.num);
+        Assert.Equal(amtSpent.denom, amtCashChange.denom);
+ 
+    }
+
+    public void Dispose()
+    {
+        _db?.Dispose();
+    }
+}
